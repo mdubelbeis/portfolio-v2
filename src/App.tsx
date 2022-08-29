dce3a53409f0627e7a3639a3a096e19e7f8e32f6
mdubelbeis <mason.dubelbeis@gmail.com>
2022-08-29 11:26:13 -0400

    added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import Header from "./Header"
+import Header from './Header';
 
 const INFO = {
-  firstName: "Mason",
-  middleName: "Jeffrey",
-  lastName: "Dubelbeis",
-  email: "Mason.Dubelbeis@gmail.com",
-  projectURLS: ['','','',''],
-
-}
+  firstName: 'Mason',
+  middleName: 'Jeffrey',
+  lastName: 'Dubelbeis',
+  email: 'Mason.Dubelbeis@gmail.com',
+  number: '732-228-2390',
+  projectURLS: ['', '', '', ''],
+  about: `I am Mason Jeffrey Dubelbeis, a Front End Web Developer. I have two years experience in working with Front End web technologies and developing beautiful responsive designs for the end user.`,
+};
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       {/* ABOUT */}
       {/* CTA/RESUME */}
       {/* PROJECTS */}
       {/* FOOTER */}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
