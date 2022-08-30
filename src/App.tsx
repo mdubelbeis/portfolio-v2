c0c196575254a655ab0059a23a5c778c59559c8c
mdubelbeis <mason.dubelbeis@gmail.com>
2022-08-29 23:08:41 -0400

    added projects logic structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,12 @@ import Heading from './components/Heading';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 
-const INFO = {
-  firstName: 'Mason',
-  middleName: 'Jeffrey',
-  lastName: 'Dubelbeis',
-  email: 'Mason.Dubelbeis@gmail.com',
-  number: '732-228-2390',
-  projectURLS: ['', '', '', ''],
-  about: `I am Mason Jeffrey Dubelbeis, a Front End Web Developer. I have two years experience in working with Front End web technologies and developing beautiful responsive designs for the end user.`,
-};
+const PROJECTS = [
+  {},
+  {},
+  {},
+  {},
+]
 
 const App: React.FC = () => {
   return (
