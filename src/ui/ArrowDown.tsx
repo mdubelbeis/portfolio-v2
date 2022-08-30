d00a17ce5c14ff003cc392723aa6639943edac0d
mdubelbeis <mason.dubelbeis@gmail.com>
2022-08-29 23:59:09 -0400

    added ArrowDown component

--- src/ui/ArrowDown.tsx
+++ src/ui/ArrowDown.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 interface ArrowDownProps {
   elementTarget: string;
+  top: string;
 }
 
-const ArrowDown: React.FC<ArrowDownProps> = ({elementTarget}) => {
+const ArrowDown: React.FC<ArrowDownProps> = ({ elementTarget, top }) => {
   return (
-    <div className="relative top-28 animate-bounce">
+    <div className={`relative ${top} animate-bounce`}>
       <a href={elementTarget}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -14,7 +15,7 @@ const ArrowDown: React.FC<ArrowDownProps> = ({elementTarget}) => {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="mt-10 h-12 w-12"
+          className="h-12 w-12"
         >
           <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
         </svg>
