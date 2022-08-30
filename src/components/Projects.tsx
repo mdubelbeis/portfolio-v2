ab1cf235d2042bd201e8d73a67f07ad442a0ab82
mdubelbeis <mason.dubelbeis@gmail.com>
2022-08-29 23:59:31 -0400

    added Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,8 @@
 const Projects: React.FC = () => {
   return (
-    <section id="projects" className="h-screen">
+    <section id="projects" className="h-screen bg-white">
       <h2 className="text-center text-3xl text-black">PROJECTS</h2>
-      <div>
-        
-      </div>
+      <div></div>
     </section>
   );
 };
