9bc054dbce5962d0e1dd6d13d2c9f33c8f0a585e
mdubelbeis <mason.dubelbeis@gmail.com>
2022-08-31 16:36:41 -0400

    corrected max-width

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Heading: React.FC = () => {
   return (
-    <header id="top" className="w-full max-w-3xl bg-black text-white opacity-90">
+    <header id="top" className="w-full max-w-6xl bg-black text-white opacity-90">
       <nav>
         <ul className="flex w-full justify-end gap-4 p-4 text-sm tracking-wide">
           <li className='hover:text-blue-500'><a href="#about-me">ABOUT</a></li>
