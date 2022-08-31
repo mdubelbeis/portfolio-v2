c43f53203c3bf450076c6e1aad13aa22b62b19cd
mdubelbeis <mason.dubelbeis@gmail.com>
2022-08-31 16:48:58 -0400

    added responsive text size for large displays

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,8 +7,8 @@ const About: React.FC = () => {
       className="flex h-screen flex-col items-center justify-center gap-10 bg-slate-100 p-4 font-hero-text leading-loose tracking-wide text-black"
     >
       <h2 className="text-center text-3xl text-black">WHO AM I?</h2>
-      <div className="md:w-9/12">
-        <p className="text-center text-lg">
+      <div className="md:w-9/12 lg:w-6/12">
+        <p className="text-center text-lg lg:text-2xl lg:leading-relaxed">
           I am a passionate self-taught frontend developer. I strive to solve any problem that presents itself and have
           a strong love working in the frontend and javascript ecosystem. If I am not adding new knowledge to my tool belt, I am
           diving deeper into the tools of the trade, allowing me to be an asset to your company.
