216bf93b58d15919dfd350a9062c89f4007d71a6
mdubelbeis <mason.dubelbeis@gmail.com>
2022-08-31 23:21:53 -0400

    added bg-opacity

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import ArrowDown from '../ui/ArrowDown';
 const Hero: React.FC = () => {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center bg-slate-800 bg-hero-pattern bg-cover bg-no-repeat text-center font-hero-text text-white bg-blend-multiply">
-      <div className="mb-24 flex h-72 w-72 flex-col justify-center gap-4 rounded-full bg-black md:h-96 md:w-96 lg:h-[32rem] lg:w-[32rem]">
+      <div className="mb-24 flex h-72 w-72 flex-col justify-center gap-4 rounded-full bg-black border border-blue-300 bg-opacity-40 border-opacity-10 md:h-96 md:w-96 lg:h-[32rem] lg:w-[32rem]">
         {/* <span className="text-3xl absolute -top-8 -left-10 -rotate-45">Hey,</span> */}
         <h1 className="font-hero-title text-6xl text-blue-500 md:text-7xl lg:text-8xl">Mason Dubelbeis</h1>
         <ul>
