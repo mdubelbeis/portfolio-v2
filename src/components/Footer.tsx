e4a0e208484255cab9dfffccbb1cd46473eec8cf
mdubelbeis <mason.dubelbeis@gmail.com>
2022-08-31 22:52:43 -0400

    added hover effects

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ const Footer: React.FC = () => {
   return (
     <footer className="flex items-center justify-center gap-4 py-4 text-white">
       <small>&copy; 2022</small>
-      <small className="hover:cursor-pointer">
-        <a href="https://mason.dubelbeis.com">Mason J. Dubelbeis</a>
+      <small className="hover:cursor-pointer hover:text-blue-500">
+        <a href="#">Mason J. Dubelbeis</a>
       </small>
     </footer>
   );
