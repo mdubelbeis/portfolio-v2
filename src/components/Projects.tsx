8cf723b6050797583c959ccc0dfd02f9c8ad3ebb
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-06 15:35:25 -0400

    corrected padding for projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -32,10 +32,10 @@ const Projects: React.FC<ProjectProps> = ({ projects }) => {
     );
   });
   return (
-    <section id="projects" className="flex flex-col items-center justify-center gap-4 bg-white px-4 text-center h-screen">
+    <section id="projects" className="flex flex-col items-center justify-center gap-4 bg-white px-4 py-20 text-center min-h-screen">
       <h2 className="mb-10 text-center font-hero-text text-4xl tracking-wider text-black lg:text-6xl">PROJECTS</h2>
       <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-6">{projectList}</div>
-      <div className="relative top-28 text-blue-500">
+      <div className="relative top-12 text-blue-500">
         <ArrowUp elementTarget="#top" />
       </div>
     </section>
