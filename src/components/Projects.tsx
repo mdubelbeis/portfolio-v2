2f73310c86a5a6c4f4fb979a710f8594ba4a40f5
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-06 14:36:42 -0400

    height adjustment

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -32,7 +32,7 @@ const Projects: React.FC<ProjectProps> = ({ projects }) => {
     );
   });
   return (
-    <section id="projects" className="flex flex-col items-center justify-center gap-4 bg-white py-20 px-4 text-center">
+    <section id="projects" className="flex flex-col items-center justify-center gap-4 bg-white py-20 px-4 text-center h-screen">
       <h2 className="mb-10 text-center font-hero-text text-4xl tracking-wider text-black lg:text-6xl">PROJECTS</h2>
       <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-6">{projectList}</div>
       <div className="relative top-10 text-blue-500">
