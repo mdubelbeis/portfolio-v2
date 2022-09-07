8d50cd026390bb918b63f8f13792520b36588d39
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-07 15:52:14 -0400

    changed order of projects array

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,6 @@ const PROJECTS = [
   },
   {
     id: 2,
-    projectLink: '#',
-    projectBgImg: '/assets/imgs/project-imgs/markus-spiske-4PG6wLlVag4-unsplash.jpg',
-    projectName: 'Du-Bee Herbs',
-    projectText: 'A landing page for local spices and herbs store.',
-    projectTech: ['React-Ts', 'Tailwind CSS'],
-  },
-  {
-    id: 3,
     projectLink: 'https://sunnyside-agency-landing-page-kappa.vercel.app/',
     projectBgImg: '/assets/imgs/project-imgs/sunny-side-agency.png',
     projectName: 'Sunny Side Agency',
@@ -31,7 +23,7 @@ const PROJECTS = [
     projectTech: ['React', 'Tailwind CSS'],
   },
   {
-    id: 4,
+    id: 3,
     projectLink: 'https://intro-section-db-menu.vercel.app/',
     projectBgImg: '/assets/imgs/project-imgs/snap.png',
     projectName: 'Snap',
@@ -39,7 +31,7 @@ const PROJECTS = [
     projectTech: ['React', 'Tailwind CSS'],
   },
   {
-    id: 5,
+    id: 4,
     projectLink: 'https://tip-calculator-rose-eight.vercel.app/',
     projectBgImg: '/assets/imgs/project-imgs/splitter.png',
     projectName: 'Splitter',
@@ -47,7 +39,7 @@ const PROJECTS = [
     projectTech: ['React', 'Tailwind CSS'],
   },
   {
-    id: 6,
+    id: 5,
     projectLink: 'https://suite-landing-page-one.vercel.app/',
     projectBgImg: '/assets/imgs/project-imgs/suite.png',
     projectName: 'Suite',
@@ -55,12 +47,20 @@ const PROJECTS = [
     projectTech: ['React', 'Tailwind CSS'],
   },
   {
-    id: 7,
+    id: 6,
     projectLink: 'https://pod-request-access-landing-page-blue.vercel.app/',
     projectBgImg: '/assets/imgs/project-imgs/pod.png',
     projectName: 'Pod',
     projectText: 'A front end web challenge from Frontendmentor.io',
     projectTech: ['HTML', 'Tailwind CSS'],
+  },
+  {
+    id: 7,
+    projectLink: '#',
+    projectBgImg: '/assets/imgs/project-imgs/markus-spiske-4PG6wLlVag4-unsplash.jpg',
+    projectName: 'Du-Bee Herbs',
+    projectText: 'A landing page for local spices and herbs store.',
+    projectTech: ['React-Ts', 'Tailwind CSS'],
   },
 ];
 
