e0255ad9606c2858b82c6bfeb00beaa73521b4a7
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-14 18:04:39 -0400

    fixed border of hero title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Hero: React.FC = () => {
   return (
     <div className="relative flex flex-col items-center justify-center bg-slate-800 bg-hero-pattern bg-cover bg-no-repeat py-32 text-center font-hero-text text-white bg-blend-multiply">
-      <div className="flex h-72 w-72 flex-col justify-center gap-4 rounded-full border border-blue-300 border-opacity-10 bg-black bg-opacity-40 md:h-96 md:w-96 lg:h-[32rem] lg:w-[32rem]">
+      <div className="flex h-72 w-72 flex-col justify-center gap-4 rounded-full border border-slate-100 border-opacity-10 bg-black bg-opacity-40 md:h-96 md:w-96 lg:h-[32rem] lg:w-[32rem]">
         <h1 className="font-hero-title text-6xl text-blue-500 md:text-7xl lg:text-8xl">Mason Dubelbeis</h1>
         <ul>
           <li className="mx-auto w-9/12 border-b-[0.05rem] border-t-[0.05rem] border-slate-200 p-2 font-hero-text text-xl md:text-2xl lg:text-3xl">
