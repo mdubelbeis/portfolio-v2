63d929a3bc14dee4831730cb6bfa91ca22eb9776
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-14 19:52:50 -0400

    hero title sizing

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero: React.FC = () => {
   return (
     <div className="h-full flex flex-col items-center justify-center py-10 bg-slate-800 bg-hero-pattern bg-cover bg-no-repeat text-center font-hero-text text-white bg-blend-multiply">
       <div className="flex flex-col justify-center gap-4">
-        <h1 className="font-hero-title text-5xl text-blue-500 md:text-7xl lg:text-8xl">Mason Dubelbeis</h1>
+        <h1 className="font-hero-title text-8xl text-blue-500">Mason Dubelbeis</h1>
         <ul>
           <li className="mx-auto w-9/12 border-b-[0.05rem] border-t-[0.05rem] border-slate-200 font-hero-text text-xl md:text-2xl lg:text-3xl">
             FRONTEND DEVELOPER
