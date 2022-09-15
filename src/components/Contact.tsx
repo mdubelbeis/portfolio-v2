08102df673da9a2a41c63243b4bb49939126559b
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-14 20:09:55 -0400

    changed color of links

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -13,22 +13,22 @@ const Contact: React.FC = () => {
           id="contact-links-wrapper"
           className={`flex gap-8 md:flex-col`}
         >
-          <li className="w-full border-[0.1rem] border-transparent shadow-xl p-2 rounded-full hover:border-green-400 text-[#0072b1] bg-transparent hover:text-green-500 active:text-green-500 focus:text-green-500">
+          <li className="w-full border-[0.1rem] border-transparent shadow-xl p-2 rounded-full hover:border-green-400 text-blue-500 bg-transparent hover:text-green-500 active:text-green-500 focus:text-green-500">
             <a href="https://www.github.com/mdubelbeis" target="_blank">
               <FaGithub size={40} />
             </a>
           </li>
-          <li className="w-full border-[0.1rem] border-transparent shadow-xl p-2 rounded-full hover:border-green-400 text-[#0072b1] hover:text-green-500 active:text-green-500 focus:text-green-500">
+          <li className="w-full border-[0.1rem] border-transparent shadow-xl p-2 rounded-full hover:border-green-400 text-blue-500 hover:text-green-500 active:text-green-500 focus:text-green-500">
             <a href="https://www.linkedin.com/in/mason-dub/" target="_blank">
               <FaLinkedin size={40} />
             </a>
           </li>
-          <li className="w-full border-[0.1rem] border-transparent shadow-xl p-2 rounded-full hover:border-green-400 text-[#1DA1F2] hover:text-green-500 active:text-green-500 focus:text-green-500">
+          <li className="w-full border-[0.1rem] border-transparent shadow-xl p-2 rounded-full hover:border-green-400 text-blue-500 hover:text-green-500 active:text-green-500 focus:text-green-500">
             <a href="https://twitter.com/mason_dubelbeis" target="_blank">
               <FaTwitter size={40} />
             </a>
           </li>
-          <li className="w-full border-[0.1rem] border-transparent shadow-xl p-2 rounded-full hover:border-green-400 text-[#0072b1] hover:text-green-500 active:text-green-500 focus:text-green-500">
+          <li className="w-full border-[0.1rem] border-transparent shadow-xl p-2 rounded-full hover:border-green-400 text-blue-500 hover:text-green-500 active:text-green-500 focus:text-green-500">
             <a href="mailto:mason.dubelbeis@gmail.com.com">
               <MdAlternateEmail size={40} />
             </a>
