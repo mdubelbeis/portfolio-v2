13bc4a9a54005a94fdad09724184866491048e7d
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-18 23:13:57 -0500

    added padding

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,34 +1,30 @@
-
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
-import { MdAlternateEmail} from 'react-icons/md';
+import { MdAlternateEmail } from 'react-icons/md';
 
 import React from 'react';
 
 const Contact: React.FC = () => {
   return (
-    <nav className={`flex justify-around mx-auto md:block md:absolute md:left-0 md:top-0 md:h-full z-10`}>
+    <nav className={`z-10 mx-auto flex justify-around md:absolute md:left-0 md:top-0 md:block md:h-full`}>
       {/* <h3 className="text-black text-5xl">CONTACT</h3> */}
-      <div id="content-container" className="md:fixed h-full py-1 shadow-xl">
-        <ul
-          id="contact-links-wrapper"
-          className={`flex gap-8 md:flex-col`}
-        >
-          <li className="w-full border-[0.1rem] border-transparent shadow-xl p-2 rounded-full hover:border-green-400 text-blue-500 bg-transparent hover:text-green-500 active:text-green-500 focus:text-green-500">
+      <div id="content-container" className="h-full py-1 px-4 shadow-xl md:fixed">
+        <ul id="contact-links-wrapper" className={`flex gap-8 md:flex-col`}>
+          <li className="w-full rounded-full border-[0.1rem] border-transparent bg-transparent p-2 text-blue-500 shadow-xl hover:border-green-400 hover:text-green-500 focus:text-green-500 active:text-green-500">
             <a href="https://www.github.com/mdubelbeis" target="_blank">
               <FaGithub size={40} />
             </a>
           </li>
-          <li className="w-full border-[0.1rem] border-transparent shadow-xl p-2 rounded-full hover:border-green-400 text-blue-500 hover:text-green-500 active:text-green-500 focus:text-green-500">
+          <li className="w-full rounded-full border-[0.1rem] border-transparent p-2 text-blue-500 shadow-xl hover:border-green-400 hover:text-green-500 focus:text-green-500 active:text-green-500">
             <a href="https://www.linkedin.com/in/mason-dub/" target="_blank">
               <FaLinkedin size={40} />
             </a>
           </li>
-          <li className="w-full border-[0.1rem] border-transparent shadow-xl p-2 rounded-full hover:border-green-400 text-blue-500 hover:text-green-500 active:text-green-500 focus:text-green-500">
+          <li className="w-full rounded-full border-[0.1rem] border-transparent p-2 text-blue-500 shadow-xl hover:border-green-400 hover:text-green-500 focus:text-green-500 active:text-green-500">
             <a href="https://twitter.com/mason_dubelbeis" target="_blank">
               <FaTwitter size={40} />
             </a>
           </li>
-          <li className="w-full border-[0.1rem] border-transparent shadow-xl p-2 rounded-full hover:border-green-400 text-blue-500 hover:text-green-500 active:text-green-500 focus:text-green-500">
+          <li className="w-full rounded-full border-[0.1rem] border-transparent p-2 text-blue-500 shadow-xl hover:border-green-400 hover:text-green-500 focus:text-green-500 active:text-green-500">
             <a href="mailto:mason.dubelbeis@gmail.com.com">
               <MdAlternateEmail size={40} />
             </a>
