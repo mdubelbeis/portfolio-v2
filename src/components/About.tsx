c258caa5749bbd6194143b92d98092e0000a9c77
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-18 23:31:23 -0500

    fixed button styling

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,12 +2,10 @@ import { useState } from 'react';
 
 const About: React.FC = () => {
   return (
-    <section
-      className="relative flex flex-col items-center justify-center gap-10 py-28 lg:gap-20 bg-slate-100 lg:py-72 px-4 font-hero-text leading-loose tracking-wide text-black"
-    >
+    <section className="relative flex flex-col items-center justify-center gap-10 bg-slate-100 py-28 px-4 font-hero-text leading-loose tracking-wide text-black lg:gap-20 lg:py-72">
       <h2 className="text-center font-hero-text text-4xl text-black lg:text-6xl">ME</h2>
       <div className="md:w-9/12 lg:w-6/12">
-        <p className="text-center lg:text-left text-xl lg:text-2xl lg:leading-relaxed">
+        <p className="text-center text-xl lg:text-left lg:text-2xl lg:leading-relaxed">
           I am a passionate self-taught <span className="lg:4xl text-2xl text-blue-500">FRONTEND DEVELOPER</span>. I
           strive to solve any problem that presents itself and committed to{' '}
           <span className="lg:4xl text-2xl text-blue-500">UI/UX DEVELOPMENT</span> and{' '}
@@ -17,7 +15,7 @@ const About: React.FC = () => {
       </div>
       <div
         id="resume"
-        className="animate-pulse mt-12 rounded-lg bg-blue-500 py-4 px-6 text-xl tracking-wider text-white drop-shadow-lg hover:animate-none hover:cursor-pointer hover:border hover:border-blue-500 hover:bg-slate-200 hover:text-blue-500 md:block lg:text-2xl"
+        className="mt-12 animate-pulse rounded-lg bg-blue-500 py-4 px-6 text-xl tracking-wider text-white shadow hover:animate-none hover:cursor-pointer hover:border hover:border-blue-500 hover:bg-white hover:text-blue-500 md:block lg:text-2xl"
       >
         <a href="/assets/resume/Resume_tech.pdf" target="_blank" rel="noopener noreferrer" download>
           CURRICULUM VITAE
