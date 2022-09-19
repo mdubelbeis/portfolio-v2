ca7b92ad98d7f5010d4e64afca6e6b27e8abd882
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-19 01:04:40 -0500

    text content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,11 +3,11 @@ import { useState } from 'react';
 const About: React.FC = () => {
   return (
     <section className="relative flex flex-col items-center justify-center gap-10 bg-slate-100 py-28 px-4 font-hero-text leading-loose tracking-wide text-black lg:gap-20 lg:py-72">
-      <h2 className="text-center font-hero-text text-4xl text-black lg:text-6xl">ME</h2>
+      <h2 className="text-center font-hero-text text-4xl text-black lg:text-6xl">WHO AM I?</h2>
       <div className="md:w-9/12 lg:w-6/12">
         <p className="text-center text-xl lg:text-left lg:text-2xl lg:leading-relaxed">
-          I am a passionate self-taught <span className="lg:4xl text-2xl text-blue-500">FRONTEND DEVELOPER</span>. I
-          strive to solve any problem that presents itself and committed to{' '}
+          A passionate self-taught <span className="lg:4xl text-2xl text-blue-500">FRONTEND DEVELOPER</span>. I strive
+          to solve any problem that presents itself and committed to{' '}
           <span className="lg:4xl text-2xl text-blue-500">UI/UX DEVELOPMENT</span> and{' '}
           <span className="lg:4xl text-2xl text-blue-500">PROBLEM SOLVING</span>. If I am not adding new knowledge to my
           tool belt, I am diving deeper into the tools of the trade.
