be9ad93113c38bff7aa7cff782fd73ff9d8b8bbf
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-26 14:31:35 -0500

    added hover features

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -14,27 +14,27 @@ const Projects: React.FC<ProjectProps> = ({ projects }) => {
     return (
       <div
         key={project.id}
-        className="mb-20 flex w-full flex-col items-center justify-center bg-white py-4 text-center lg:mb-0 lg:py-6"
+        className="mb-20 flex w-full flex-col items-center justify-center bg-white text-center shadow-lg hover:shadow-xl active:shadow-lg lg:mb-0"
       >
         <a
           href={project.projectLink}
-          className="hover:bg-slate-200 hover:text-blue-800 hover:outline-cyan-700 focus:bg-slate-200 focus:text-blue-800 focus:outline-cyan-700 active:bg-slate-200 active:text-blue-800 active:outline-cyan-700"
+          className="hover:text-blue-800 hover:outline-cyan-700 focus:text-blue-800 focus:outline-cyan-700 active:text-blue-800 active:outline-cyan-700"
           target="_blank"
         >
-          <div className="flex flex-col items-center justify-center">
+          <div className="flex max-h-fit flex-col items-center justify-center">
             <img className="w-full rounded-lg rounded-b-none" src={project.projectBgImg} alt={project.projectName} />
-            <div className="w-full space-y-2 border-[1px] border-t-0 border-white py-6 px-4 shadow-xl">
+            <div className="w-full space-y-2 border-[1px] border-t-0 border-white py-8 px-4">
               <p className="font-cursive text-2xl font-thin tracking-wider text-blue-500 underline underline-offset-4 hover:text-blue-700 md:text-3xl">
                 {project.projectName}
               </p>
-              <p className="px-8 font-hero-text tracking-wider">{project.projectText}</p>
-              <ul className="flex items-center justify-center gap-4 font-hero-text">
-                {project.projectTech.map((tech) => {
-                  return <li className="p-6 tracking-wider text-blue-800">{tech}</li>;
-                })}
-              </ul>
+              <p className="px-8 font-hero-text">{project.projectText}</p>
             </div>
           </div>
+          <ul className="mb-2 flex items-center justify-center gap-4 font-hero-text">
+            {project.projectTech.map((tech) => {
+              return <li className="tracking-wide text-blue-800">{tech}</li>;
+            })}
+          </ul>
         </a>
       </div>
     );
