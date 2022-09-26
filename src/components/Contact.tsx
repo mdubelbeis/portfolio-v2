486ce1d92c6e16461f44d51a5dd3c8fb2d530473
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-26 14:18:18 -0500

    edit links

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -12,17 +12,17 @@ const ICONS = [
 
 const Contact: React.FC = () => {
   return (
-    <nav className={`z-10 mx-auto flex justify-around pt-6 md:absolute md:left-0 md:top-0 md:block md:h-full`}>
+    <nav className={`z-10 mx-auto flex justify-around py-4 md:absolute md:left-0 md:top-0 md:block md:h-full md:pt-6`}>
       {/* <h3 className="text-black text-5xl">CONTACT</h3> */}
       <div id="content-container" className="h-full py-1 px-4 shadow-xl md:fixed">
-        <ul id="contact-links-wrapper" className={`flex gap-8 md:flex-col`}>
+        <ul id="contact-links-wrapper" className={`flex gap-10 md:flex-col`}>
           {ICONS.map((icon) => {
             return (
               <li
                 key={Math.random()}
-                className="w-full rounded-full border-[0.1rem] border-blue-500 border-b-transparent border-t-transparent border-opacity-60 p-2 text-blue-500 shadow-xl hover:border-blue-700 hover:border-r-transparent hover:border-l-transparent hover:border-opacity-60 hover:text-blue-700 focus:border-blue-700 focus:border-r-transparent focus:border-l-transparent focus:border-opacity-60 focus:text-blue-700 focus:outline-none active:border-blue-700 active:border-r-transparent active:border-l-transparent active:border-opacity-60 active:text-blue-700"
+                className="w-full rounded-full border-opacity-60 p-4 text-blue-500 shadow-lg hover:text-blue-700 hover:shadow-xl focus:border-opacity-60 focus:text-blue-700 focus:outline-none active:shadow-lg"
               >
-                <a href={icon.url} target="_blank" className="focus:outline-cyan-700">
+                <a href={icon.url} target="_blank" className="">
                   {icon.icon}
                 </a>
               </li>
