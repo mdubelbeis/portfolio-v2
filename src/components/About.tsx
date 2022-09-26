97a634e0a23e728f097d0d31f4f65b79bf5d4f65
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-26 10:24:20 -0500

    fixed line-height

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,16 +1,22 @@
 import { useState } from 'react';
 
 const About: React.FC = () => {
+  const spanText = 'lg:4xl text-2xl text-blue-500';
   return (
-    <section className="relative flex flex-col items-center justify-center gap-10 bg-slate-100 py-28 px-4 font-hero-text leading-loose tracking-wide text-black lg:gap-20 lg:py-72">
+    <section className="relative flex flex-col items-center justify-center gap-10 bg-slate-100 py-28 px-10 font-hero-text leading-loose tracking-wide text-black lg:gap-20 lg:py-72">
       <h2 className="text-center font-hero-text text-4xl text-black lg:text-6xl">WHO AM I?</h2>
-      <div className="md:w-9/12 lg:w-6/12">
-        <p className="text-center text-xl lg:text-2xl lg:leading-relaxed">
-          A passionate self-taught <span className="lg:4xl text-2xl text-blue-500">FRONTEND DEVELOPER</span>. I strive
-          to solve any problem that presents itself and committed to{' '}
-          <span className="lg:4xl text-2xl text-blue-500">UI/UX DEVELOPMENT</span> and{' '}
-          <span className="lg:4xl text-2xl text-blue-500">PROBLEM SOLVING</span>. If I am not adding new knowledge to my
-          tool belt, I am diving deeper into the tools of the trade.
+      <div className="flex flex-col text-center text-xl md:w-9/12 lg:text-2xl">
+        <p className="leading-10">
+          I am a passionate self-taught <span className={spanText}>FRONT-END WEB DEVELOPER</span> based out of Austin,
+          TX, with a focus on <span className={spanText}>JAVASCRIPT/TYPESCRIPT, REACT, and CSS/SASS/TailwindCSS</span>.
+          I strive to solve any problem that presents itself and committed to{' '}
+          <span className={spanText}>UI/UX DEVELOPMENT</span> and <span className={spanText}>PROBLEM SOLVING</span>.
+          Dedicated professional with over 8 years of outstanding performance and results for the United States Navy.
+          Earned three promotions rapidly and excelled as a <span className={spanText}>LEADER</span>, leading over 500
+          Sailors in daily operations and in support of the Navy's mission. Seeking to provide your company with an
+          <span className={spanText}> OUTSTANDING TEAM MEMBER</span> that will hit the ground running and earning a spot
+          as an
+          <span className={spanText}> ASSET</span> for your company.
         </p>
       </div>
       <div
