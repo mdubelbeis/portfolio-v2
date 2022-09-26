5f7bddcff2daa9c7a4f0cb30fc5574b2801b7d26
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-26 12:38:06 -0500

    restructured and edit text content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,18 +5,21 @@ const About: React.FC = () => {
   return (
     <section className="relative flex flex-col items-center justify-center gap-10 bg-slate-100 py-28 px-10 font-hero-text leading-loose tracking-wide text-black lg:gap-20 lg:py-72">
       <h2 className="text-center font-hero-text text-4xl text-black lg:text-6xl">WHO AM I?</h2>
-      <div className="flex flex-col text-center text-xl md:w-9/12 lg:text-2xl">
+      <div className="flex flex-col gap-6 text-center text-xl md:w-9/12 lg:text-2xl">
         <p className="leading-10">
-          I am a passionate self-taught <span className={spanText}>FRONT-END WEB DEVELOPER</span> based out of Austin,
-          TX, with a focus on <span className={spanText}>JAVASCRIPT/TYPESCRIPT, REACT, and CSS/SASS/TailwindCSS</span>.
-          I strive to solve any problem that presents itself and committed to{' '}
-          <span className={spanText}>UI/UX DEVELOPMENT</span> and <span className={spanText}>PROBLEM SOLVING</span>.
-          Dedicated professional with over 8 years of outstanding performance and results for the United States Navy.
-          Earned three promotions rapidly and excelled as a <span className={spanText}>LEADER</span>, leading over 500
-          Sailors in daily operations and in support of the Navy's mission. Seeking to provide your company with an
+          I am a dedicated and passionate self-taught <span className={spanText}>FRONT-END WEB DEVELOPER</span> based
+          out of Austin, TX, with a focus on{' '}
+          <span className={spanText}>JAVASCRIPT/TYPESCRIPT, REACT, and CSS/SASS/TailwindCSS</span>. I strive to solve
+          any problem that presents itself and committed to <span className={spanText}>UI/UX DEVELOPMENT</span> and{' '}
+          <span className={spanText}>PROBLEM SOLVING</span>. Navy Veteran with over 8 years of outstanding performance
+          and results for the United States Navy. Rapidly promoted and excelled as a{' '}
+          <span className={spanText}>LEADER</span>, leading over 500 Sailors in daily operations and in support of the
+          Navy's mission.
+        </p>
+        <p>
+          I will provide your company with an
           <span className={spanText}> OUTSTANDING TEAM MEMBER</span> that will hit the ground running and earning a spot
-          as an
-          <span className={spanText}> ASSET</span> for your company.
+          as an <span className={spanText}> ASSET</span> for your company.
         </p>
       </div>
       <div
