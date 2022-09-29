11bc4fde789f0579721d694e7dfa831276261f1e
mdubelbeis <mason.dubelbeis@gmail.com>
2022-09-28 21:30:06 -0500

    added text-alignment and size

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,12 +5,12 @@ const About: React.FC = () => {
   return (
     <section className="relative flex flex-col items-center justify-center gap-10 bg-slate-100 py-28 font-hero-text leading-loose tracking-wide text-black md:px-8 lg:gap-20 lg:py-72">
       <h2 className="text-center font-hero-text text-4xl text-black lg:text-6xl">WHO AM I?</h2>
-      <div className="text-md flex w-full flex-col gap-6 px-2 text-center md:w-9/12 md:text-xl lg:text-2xl">
+      <div className="text-md flex flex-col gap-6 px-4 text-left md:w-9/12 md:text-xl lg:text-2xl">
         <p className="leading-6 lg:leading-10">
           I am a dedicated and passionate self-taught <span className={spanText}>FRONT-END WEB DEVELOPER</span> based
-          out of Austin, TX, with a focus on{' '}
-          <span className={spanText}>JAVASCRIPT/TYPESCRIPT, REACT, and CSS/SASS/TailwindCSS</span>. I strive to solve
-          any problem that presents itself and committed to <span className={spanText}>UI/UX DEVELOPMENT</span> and{' '}
+          out of Austin, TX, with a focus on <span className={spanText}>JAVASCRIPT/TYPESCRIPT, REACT, CSS, SASS</span>,
+          and <span className={spanText}>TAILWIND CSS</span>. I strive to solve any problem that presents itself and
+          committed to <span className={spanText}>UI/UX DEVELOPMENT</span> and{' '}
           <span className={spanText}>PROBLEM SOLVING</span>. Navy Veteran with over 8 years of outstanding performance
           and results for the United States Navy. Rapidly promoted and excelled as a{' '}
           <span className={spanText}>LEADER</span>, leading over 500 Sailors in daily operations and in support of the
