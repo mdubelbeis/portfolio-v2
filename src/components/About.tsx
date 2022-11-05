43932e011acdeab5f6330b8a97719a484e945595
mdubelbeis <mason.dubelbeis@gmail.com>
2022-11-05 00:51:52 -0500

    fixed leading issue

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import Button from '../ui/Button';
 const About: React.FC = () => {
   const spanText = 'text-blue-500 lowercase';
   return (
-    <section className="relative flex flex-col items-center justify-center gap-10 bg-slate-100 py-28 font-hero-text leading-loose tracking-wide text-black md:px-8 lg:gap-20 lg:py-72">
+    <section className="relative flex flex-col items-center justify-center gap-10 bg-slate-100 py-28 font-hero-text tracking-wide text-black md:px-8 lg:gap-20 lg:py-72">
       <h2 className="text-center font-hero-text text-4xl text-black lg:text-6xl">WHO AM I?</h2>
       <div className="text-md flex flex-col gap-6 px-4 text-left md:w-9/12 md:text-xl lg:text-2xl">
         <p className="leading-6 lg:leading-10">
@@ -17,9 +17,8 @@ const About: React.FC = () => {
           Navy's mission.
         </p>
         <p>
-          I will provide your company with an
-          <span className={spanText}> OUTSTANDING TEAM MEMBER</span> who will hit the ground running and earn a spot as
-          an <span className={spanText}> ASSET</span> to your company.
+          I will provide your company with an <span className={spanText}> OUTSTANDING TEAM MEMBER</span> who will hit
+          the ground running and earn a spot as an <span className={spanText}> ASSET</span> to your company.
         </p>
       </div>
       <Button py="py-4" px="px-6" textSize="text-xl" marginTop="mt-10">
