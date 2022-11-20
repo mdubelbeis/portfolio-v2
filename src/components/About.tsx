813f118877791b89b7565fd0fde68ba633462684
mdubelbeis <mason.dubelbeis@gmail.com>
2022-11-20 14:46:57 -0600

    changed layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,22 +1,27 @@
 import Button from '../ui/Button';
 
 const About: React.FC = () => {
-  const spanText = 'text-blue-500 lowercase';
   return (
     <section className="relative flex flex-col items-center justify-center gap-10 bg-slate-100 py-32 font-hero-text tracking-wide text-black md:px-8 lg:gap-20 lg:py-72">
-      <h2 className="text-center font-hero-text text-4xl text-black md:text-5xl lg:text-7xl">WHO AM I?</h2>
-      <div className="text-md flex flex-col gap-6 px-4 text-left md:w-9/12 md:text-xl lg:text-2xl">
-        <p className="leading-6 lg:leading-10">
-          I am a dedicated and passionate self-taught <span className={spanText}>FRONT-END WEB DEVELOPER</span> based
-          out of Austin, TX, with a focus on <span className={spanText}>JAVASCRIPT/TYPESCRIPT, REACT, CSS, SASS</span>,
-          and <span className={spanText}>TAILWIND CSS</span>. I strive to solve any problem that presents itself and
-          committed to <span className={spanText}>UI/UX DEVELOPMENT</span> and{' '}
-          <span className={spanText}>PROBLEM SOLVING</span>.
-        </p>
-        <p>
-          I will provide your company with an <span className={spanText}> OUTSTANDING TEAM MEMBER</span> who will hit
-          the ground running and earn a spot as an <span className={spanText}> ASSET</span> to your company.
-        </p>
+      <h2 className="text-center font-hero-text text-4xl text-black md:text-5xl lg:text-7xl">ABOUT</h2>
+      <div className="text-md flex flex-col gap-10 px-4 text-left md:w-9/12 md:text-xl lg:text-2xl">
+        <div className="flex flex-col gap-2">
+          <h1>I am a...</h1>
+          <h3>Web and Javascript Developer.</h3>
+          <h3>United States Navy Veteran based of of Austin, TX.</h3>
+        </div>
+        <div className="flex flex-col gap-4 leading-relaxed">
+          <p>
+            My passion for development stems from my interest in problem solving. Solving problems and accomplishing
+            challenges continue to be my primary motivation to this day.
+          </p>
+
+          <p>
+            Outside of work and working on personal projects, I thoroughly enjoy learning new things, spending quality
+            time with family and friends, not spending money on maintaining a Harley Davidson, and supporting local
+            Veterans.
+          </p>
+        </div>
       </div>
       <Button py="py-4" px="px-6" textSize="text-xl" marginTop="mt-10">
         CURRICULUM VITAE
