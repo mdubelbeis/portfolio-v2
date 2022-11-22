3538b6b33d106f25375925b3b64924ec85fc82c8
mdubelbeis <mason.dubelbeis@gmail.com>
2022-11-22 10:20:53 -0600

    added new custom bg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,10 +8,9 @@ module.exports = {
       },
       backgroundImage: {
         'hero-pattern': "url('/assets/imgs/christian-wiediger-WkfDrhxDMC8-unsplash.jpg')",
-        'blue-smoke': "url('/assets/imgs/pawel-czerwinski-YWIOwHvRBvU-unsplash.jpg')",
+        code: "url('public/assets/imgs/ilya-pavlov-OqtafYT5kTw-unsplash.jpg')",
         'blue-abstraction': "url('/assets/imgs/max-kleinen-ohQinqTl_DE-unsplash.jpg')",
         'blue-reaction': "url('/assets/imgs/engin-akyurt-8eLBp2xo68c-unsplash.jpg')",
-        'coding-bg': 'url(/assets/imgs/coding-bg)',
       },
       fontFamily: {
         lato: ['Lato', 'sans-serif'],
