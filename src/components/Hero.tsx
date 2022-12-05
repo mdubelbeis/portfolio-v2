32548c72e790ae98cf22ee9473ed1b5b4ad6964e
mdubelbeis <mason.dubelbeis@gmail.com>
2022-12-05 08:20:37 -0600

    restyled

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Hero: React.FC = () => {
   return (
-    <div className="font-hero-text flex h-full flex-col items-center justify-center bg-slate-800 bg-code bg-cover bg-center bg-no-repeat py-24 text-center text-white bg-blend-multiply">
-      <div className="flex flex-col items-center justify-center gap-6">
+    <div className="font-hero-text flex flex-col items-center justify-center bg-slate-800 bg-code bg-cover bg-center bg-no-repeat px-10 py-28 text-center text-white bg-blend-multiply">
+      <div className="flex w-full flex-col gap-6">
         <h1 className="font-josefin-sans text-6xl font-bold tracking-wider text-blue-500 lg:text-8xl">
           Mason <span className="md:block">Dubelbeis</span>
         </h1>
         <ul>
-          <li className="font-hero-text mx-auto border-b-[0.05rem] border-t-[0.05rem] border-blue-500 py-2 text-2xl tracking-wider md:w-full lg:text-5xl">
+          <li className="font-hero-text mx-auto w-9/12 border-b-[0.05rem] border-t-[0.05rem] border-blue-500 py-2 text-2xl tracking-wider md:w-6/12 lg:text-5xl">
             WEB DEVELOPER
           </li>
         </ul>
