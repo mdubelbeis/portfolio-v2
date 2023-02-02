dbb9782859a447616f0ef0eb097a10e8d35f67c7
mdubelbeis <mason.dubelbeis@gmail.com>
2023-02-01 19:01:31 -0600

    removed title and restyled

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,21 +2,22 @@ import Button from '../ui/Button';
 
 const About: React.FC = () => {
   return (
-    <section className="font-hero-text flex flex-col items-center justify-center gap-14 bg-slate-100 py-32 tracking-wider text-black md:px-10 lg:gap-20 lg:py-72">
-      <div className="font-hero-text flex flex-col items-center justify-center gap-14 md:px-14 lg:flex-row-reverse lg:gap-20">
-        <h2 className="font-hero-text text-center text-5xl text-black lg:text-7xl">ABOUT</h2>
-        <div className="hidden lg:block lg:h-[250px] lg:border-l-2 lg:border-black lg:opacity-30"></div>
+    <section className="font-hero-text flex flex-col items-center justify-center gap-6 bg-slate-100 py-32 tracking-wider text-black lg:gap-10 lg:py-72 lg:tracking-wider">
+      <div className="font-hero-text flex flex-col items-center justify-center lg:flex-row-reverse">
         <div className="flex max-w-2xl flex-col items-center justify-center gap-2 p-4 text-left text-lg">
-          <div className="flex flex-col gap-6 px-1 md:ml-10">
-            <h3 className="text-2xl font-bold md:text-3xl">Hello, I'm Mason!</h3>
-            <p>I am a United States Navy Veteran, Web, and Javascript Developer.</p>
-            <p>I build websites, apps, and features for consumers to interact with your business.</p>
-            <em className="mt-8 text-center text-lg font-semibold">Let me help your business grow.</em>
+          <div className="flex flex-col gap-6 px-1 text-center font-mono md:m-10 md:text-right lg:m-0 lg:gap-10 lg:text-left lg:text-2xl">
+            <h3 className="text-2xl font-bold md:text-3xl lg:text-6xl">Hello, I'm Mason!</h3>
+            <p>A Full Stack Software Engineer.</p>
+            <p>
+              I have a passion for creating beautiful, interactive, type safe websites and/or applications that are easy
+              to use, look great, and vibe with end-users, while ensuring a readable, type-safe codebase for future
+              maintainers.
+            </p>
           </div>
         </div>
       </div>
       <div className="mx-auto">
-        <Button py="py-4" px="px-6" textSize="text-xl" marginTop="mt-10">
+        <Button py="py-4" px="px-6" textSize="text-xl" marginTop="mt-4">
           RESUME
         </Button>
       </div>
