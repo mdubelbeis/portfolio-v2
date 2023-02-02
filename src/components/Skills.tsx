c7103031bd554736c28c6590772591c3a122f78a
mdubelbeis <mason.dubelbeis@gmail.com>
2023-02-01 19:02:15 -0600

    removed title

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -17,7 +17,7 @@ const Skills: React.FC = () => {
   return (
     <section className="xl:gap-26 relative flex w-full flex-col items-center justify-start bg-slate-800 bg-blue-reaction bg-cover bg-center bg-no-repeat px-4 py-32 text-white bg-blend-soft-light lg:flex-row lg:items-center lg:justify-center lg:gap-20 lg:px-28">
       <div className="p-4">
-        <h2 className="font-hero-text mb-2 text-5xl tracking-widest text-white lg:text-7xl">SKILLS</h2>
+        <h2 className="font-hero-text mb-2 text-5xl tracking-widest text-white lg:text-center lg:text-6xl">TOOLBELT</h2>
       </div>
       <div className="hidden lg:block lg:h-[250px] lg:border-l-2 lg:border-white lg:opacity-30"></div>
       <div className="mt-16 grid grid-cols-3 gap-20 p-8 lg:mt-0 xl:grid-cols-4">
