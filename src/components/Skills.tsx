51275e06c3a964d77d7021a226091c9ecb352d01
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-06-20 15:16:52 -0500

    removed images

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -19,7 +19,7 @@ const ICONS = [
 
 const Skills: React.FC = () => {
   return (
-    <section className="xl:gap-26 relative flex w-full flex-col items-center justify-start bg-slate-800 bg-blue-reaction bg-cover bg-center bg-no-repeat px-4 py-32 text-white bg-blend-soft-light lg:flex-row lg:items-center lg:justify-center lg:gap-20 lg:px-28">
+    <section className="xl:gap-26 relative flex w-full flex-col items-center justify-start bg-slate-800 px-4 py-32 text-white bg-blend-soft-light lg:flex-row lg:items-center lg:justify-center lg:gap-20 lg:px-28">
       <div className="p-4">
         <h2 className=" lg:text-6x mb-2 font-mono text-5xl tracking-widest text-white lg:text-center">TOOLBELT</h2>
       </div>
