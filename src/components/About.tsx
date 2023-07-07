f2152ffc7c7c1efa2b3b7e7df56b53cca6efa5d3
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2023-07-07 09:41:57 -0500

    fixed height

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import Button from '../ui/Button';
 
 const About: React.FC = () => {
     return (
-        <section className="font-hero-text flex flex-col items-center justify-center gap-6 bg-slate-100 py-32 tracking-wider text-black lg:gap-10 lg:tracking-wider">
+        <section className="font-hero-text flex h-screen flex-col items-center justify-center gap-6 bg-slate-100 py-32 tracking-wider text-black lg:gap-10 lg:tracking-wider">
             <div className="font-hero-text flex flex-col items-center justify-center lg:flex-row-reverse">
                 <div className="flex flex-col items-center justify-center gap-2 p-4 text-lg">
                     <div className="flex flex-col gap-6 px-1 text-center font-mono md:m-10 lg:m-0 lg:gap-10 lg:text-2xl">
